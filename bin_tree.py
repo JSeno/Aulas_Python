dd9d9b00520c37924452ddb227d87c21526c1dbd
jefferson.seno <jefferson.seno@datacake.com.br>
2021-12-20 10:59:41 -0300

    Enviando

---
+++ bin_tree.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return str(self.data)
+
+
+class BinaryTree:
+    def __init__(self, data=None):
+        if data:
+            node = Node(data)
+            self.root = node
+        else:
+            self.root = None
+
+
+if __name__ == "main":
+    tree = BinaryTree(7)
+    tree.root.left = Node(18)
+    tree.root.right = Node(14)
+
+    print(tree.root)
+    print(tree.root.right)
+    print(tree.root.left)
--- generators.py
+++ generators.py
@@ -15,5 +15,52 @@ nomes = ['Calors', 'Camila', 'Carla', 'Cassiano', 'Cristina', 'Vanessa']
 # Poderiamos ter feito utilizando Generator
 
 
-nomes = ['Calors', 'Camila', 'Carla', 'Cassiano', 'Cristina', 'Vanessa']
-print(any(nome[0] == 'C' for nome in nomes))
+# nomes = ['Calors', 'Camila', 'Carla', 'Cassiano', 'Cristina', 'Vanessa']
+# print(any(nome[0] == 'C' for nome in nomes))
+
+# # list Comprehension
+# res = [nome[0] == 'C' for nome in nomes]
+# print(type(res))
+# print(res)
+
+# res = (nome[0] == 'C' for nome in nomes)
+# print(type(res))
+# print(res)
+
+
+# Qual é a utilidade de getsizeof? Retorna a quantidade de bits em memória do elemento passado como parâmetro
+from sys import getsizeof
+
+# print(getsizeof('geek'))
+# print(getsizeof('university'))
+# print(getsizeof(12))
+# print(getsizeof(1124))
+# print(getsizeof(15134111634))
+# print(getsizeof(False))
+
+# Gerando uma lista de números com List Comprehension
+# list_comp = getsizeof([x * 10 for x in range(1000)])
+
+# # Gerando uma lista de números com List Comprehension
+# set_comp = getsizeof({x * 10 for x in range(1000)})
+
+# # Gerando uma lista de números com List Comprehension
+# dic_comp = getsizeof({x:x * 10 for x in range(1000)})
+
+# # Gerando uma lista de números com Generator
+# gen = getsizeof(x * 10 for x in range(1000))
+
+# print('Para fazer a mesma tarefa gastamos em memória: ')
+# print(f'List Comprehension: {list_comp} bytes')
+# print(f'Set Comprehension: {set_comp} bytes')
+# print(f'Dictionary Comprehension: {dic_comp} bytes')
+# print(f'Generator Comprehension: {gen} bytes')
+
+# Eu posso interar no Generator Expression? SIm
+gen = (x * 10 for x in range(1000))
+print(gen)
+print(type(gen))
+
+for num in gen:
+    print(num)
+    
